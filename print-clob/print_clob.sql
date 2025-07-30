b44b4e6b2b65f39d7958cec6b1dc1430999030df
Abdulrahman Khalifa <121583217+abdukhalifa92@users.noreply.github.com>
2025-07-30 19:09:28 +0400

    Add files via upload

--- print-clob/print_clob.sql
+++ print-clob/print_clob.sql
@@ -1,1 +1,18 @@
-
+create or replace PROCEDURE PRINT_CLOB (P_CLOB IN CLOB)
+   IS
+      L_BUFFER   VARCHAR2 (32767);
+      L_AMOUNT   BINARY_INTEGER := 32767;
+      L_POSTION  INTEGER := 1;
+   BEGIN
+      --__________________________________________________________________________________________________--
+      LOOP
+         DBMS_LOB.READ (P_CLOB,
+                        L_AMOUNT,
+                        L_POSTION,
+                        L_BUFFER);
+         DBMS_OUTPUT.PUT_LINE (L_BUFFER);
+         L_POSTION := L_POSTION + L_AMOUNT;
+         EXIT WHEN L_POSTION > DBMS_LOB.GETLENGTH (P_CLOB);
+      END LOOP;
+   --__________________________________________________________________________________________________--
+   END;
